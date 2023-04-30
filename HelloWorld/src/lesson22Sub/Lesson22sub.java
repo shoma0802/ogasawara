bdf87a67c43e225a3245114048de36b1413d23da
ogasawarashouba <ogasawarashouba@localhost>
2023-04-30 21:51:46 +0900

    課題9,22修正版

---
+++ HelloWorld/src/lesson22Sub/Lesson22sub.java
@@ -0,0 +1,14 @@
+package lesson22Sub;
+
+import lesson22.Lesson22;
+
+public class Lesson22sub {
+    public static void main(String[] args) {
+        Lesson22 lesson = new Lesson22();
+
+        lesson.sayHello();
+        lesson.saySushiTastesGood();
+        lesson.japaneseFood();
+        lesson.showCurrentDateTime();
+    }
+}
