48ad30f6b8c9c71384732b2cd858348a3edf2e61
ogasawarashouba <ogasawarashouba@localhost>
2023-05-24 21:27:47 +0900

    課題33修正

--- HelloWorld/src/lesson33/Person.java
+++ HelloWorld/src/lesson33/Person.java
@@ -1,38 +1,47 @@
 package lesson33;
 
 class Person {
+	// 合計人数を保持する変数
     public static int count = 0; 
     public String firstName; 
     public String lastName; 
     public int age; 
     public double height, weight; 
-
+    
+    // インスタンスのフィールドを初期化するコンストラクタ
     Person(String firstName, int age, double height, double weight) {
-        Person.count++; // 問題3: インスタンス生成ごとに合計人数をインクリメント
+    	// 問題3: インスタンス生成ごとに合計人数をインクリメント
+        Person.count++; 
         this.firstName = firstName;
         this.age = age;
         this.height = height;
         this.weight = weight;
     }
-
+    
+    // インスタンスのフィールドを初期化し、lastNameフィールドの値をセットするコンストラクタ
     Person(String firstName, String lastName, int age, double height, double weight) {
         this(firstName, age, height, weight); 
-        this.lastName = lastName; // 問題2: lastNameフィールドの値をセット
+        // 問題2: lastNameフィールドの値をセット
+        this.lastName = lastName; 
     }
-
+    
+    // フルネームを返すメソッド
     public String fullName() {
         return this.firstName + " " + this.lastName; 
     }
-
+    
+    // 名前と年齢を表示するメソッド
     public void print() {
         System.out.println("名前は" + this.fullName() + "です"); 
         System.out.println("年は" + this.age + "才です"); 
     }
 
+    // BMIを計算して返すメソッド
     public double bmi() {
         return this.weight / (this.height * this.height);
     }
 
+    // 合計人数を表示するメソッド
     public static void printCount() {
         System.out.println("合計" + Person.count + "人です"); 
     }
