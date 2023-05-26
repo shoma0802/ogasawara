29f3f4937c2131f7c97283f8fddbd86c4e466c52
ogasawarashouba <ogasawarashouba@localhost>
2023-05-26 21:03:18 +0900

    課題32　修正

--- HelloWorld/src/lesson32/Main.java
+++ HelloWorld/src/lesson32/Main.java
@@ -3,11 +3,14 @@ package lesson32;
 class Main {
     public static void main(String[] args) {
         Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+        // 鈴木太郎の情報を表示
         person1.print();
-
+  
         Person person2 = new Person("山田花子", 22, 1.5, 40);
+        // 山田花子の情報を表示
         person2.print();
-
-        Person.printCount(); // 問題6
+        
+        // 問題6: 現在の「count」の値を表示
+        Person.printCount(); 
     }
 }
--- HelloWorld/src/lesson32/Person.java
+++ HelloWorld/src/lesson32/Person.java
@@ -1,22 +1,23 @@
 package lesson32;
 
 class Person {
+	//それぞれの情報のセット
     public String name;
     public int age;
     public double height; 
     public double weight;
-
-    public static int count; // 問題1
+    // 問題1: 静的変数「count」を定義
+    public static int count; 
 
     Person(String name, int age, double height, double weight) {
         this.name = name;
         this.age = age;
         this.height = height;
         this.weight = weight;
-
-        count++; // 問題2
+        // 問題2: インスタンスが生成されるごとに「count」をインクリメント
+        count++; 
     }
-
+    // BMIを計算するメソッド
     public double bmi() {
         return this.weight / (this.height * this.height);
     }
@@ -25,8 +26,9 @@ class Person {
         System.out.println("名前は" + this.name + "です"); 
         System.out.println("年は" + this.age + "です"); 
     }
-
-    public static void printCount() { // 問題4
-        System.out.println("合計" + count + "人です"); // 問題5
+    // 問題4: 静的メソッド「printCount()」を定義
+    public static void printCount() { 
+    	// 問題5: 現在の「count」の値を表示
+        System.out.println("合計" + count + "人です"); 
     }
 }
